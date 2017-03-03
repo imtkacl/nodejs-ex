12651362af2b28adce8e328d4fb668d4632a084e
Kam Chiu Leung <kam_leung@cathaypacific.com>
2017-03-03 16:34:19 +0800

    Refactor out below function extractUsernameFromRequest extractPasswordFromRequest assertUsernamePassword assertUserSearchInfoError createOnLdapBindHandler createOnLdapSearchHandler createOnLdapSearchEntryHandler createOnBindLoginUserHandler

--- server.js
+++ server.js
@@ -78,6 +78,34 @@ app.get('/testBackEnd/', function (req, res) {
 
 	});
 });
+function extractUsernameFromRequest(req) {
+	var loginUsername = null;
+	if (req.body.username != null) {
+		loginUsername = req.body.username;
+	} else if (req.headers.username != null) {
+		loginUsername = req.headers.username;
+	}
+	return loginUsername;
+}
+
+function extractPasswordFromRequest(req) {
+	var loginPassword = null;
+	if (req.body.password != null) {
+		loginPassword = req.body.password;
+	} else if (req.headers.password != null) {
+		loginPassword = req.headers.password;
+	}
+	return loginPassword;
+}
+
+function assertUsernamePassword(userSearchInfo) {
+	if (loginUsername == null || loginPassword == null) {
+		userSearchInfo.errorMessage = 'Invalid username: ' + userSearchInfo.loginUsername + ' or password ' + userSearchInfo.loginPassword;
+		console.log(userSearchInfo.errorMessage);
+		return false;
+	}
+	return true;
+}
 
 function unbindLdap(client) {
 	client.unbind(function (error) {
@@ -87,6 +115,87 @@ function unbindLdap(client) {
 			console.log('client disconnected');
 		}
 	});
+}
+
+function createOnLdapBindHandler(client, res, systemUserName, userSearchInfo) {
+	return function (error) {
+		if (error) {
+			console.log('Unable to bind with systemUsername: ' + systemUserName + ' with error ' + error.message);
+			unbindLdap(clietn);
+			res.send('Unable to bind with systemUsername: ' + systemUserName + ' with error ' + error.message);
+		} else {
+			console.log('connected');
+			var opts = {
+				filter: '(cn=' + userSearchInfo.loginUsername + ')',
+				scope: 'sub',
+				attributes: ['dn']
+			};
+			client.search(userSearchInfo.loginBaseDn, opts, createOnLdapSearchHandler(client, res, userSearchInfo));
+		}
+	}
+}
+
+function createOnLdapSearchHandler(client, res, userSearchInfo) {
+	return function (error, search) {
+		console.log('Searching for ' + userSearchInfo.loginUsername);
+		if (error) {
+			userSearchInfo.errorMessage = 'Unable to search with loginUsername: ' + userSearchInfo.loginUsername + ' with error ' + error.message;
+			console.log(userSearchInfo.errorMessage);
+			unbindLdap(client);
+			res.send(userSearchInfo.errorMessage);
+		} else {
+			search.on('searchEntry', createOnLdapSearchEntryHandler(userSearchInfo));
+			search.on('error', createOnLdapSearchErrorHandler(userSearchInfo));
+			search.on('end', createOnLdapSearchEndHandler(client, res, userSearchInfo));
+		}
+	}
+}
+
+function createOnLdapSearchEntryHandler(userSearchInfo) {
+	return function (entry) {
+		if (entry.object) {
+			userSearchInfo.loginUserDnCount++;
+			if (userSearchInfo.loginUserDnCount == 1) {
+				userSearchInfo.loginUserDn = entry.object.dn;
+			}
+			console.log('Found DN: ' + JSON.stringify(entry.object));
+		}
+	}
+}
+
+function createOnLdapSearchEndHandler(client, res, userSearchInfo) {
+	return function (result) {
+		if (!assertUserSearchInfoError(userSearchInfo, result)) {
+			unbindLdap(client);
+			res.send(userSearchInfo.errorMessage);
+		} else {
+			console.log('Binding using DN: ' + userSearchInfo.loginUserDn);
+			client.bind(userSearchInfo.loginUserDn, userSearchInfo.loginPassword, createOnBindLoginUserHandler(client, res, userSearchInfo));
+		}
+	}
+}
+
+function assertUserSearchInfoError(userSearchInfo, result) {
+	if (userSearchInfo.errorMessage == null) {
+		if (result.status != 0) {
+			userSearchInfo.errorMessage = 'error in searching with loginUsername: ' + userSearchInfo.loginUsername + ' with status' + result.status;
+		} else if (userSearchInfo.loginUserDnCount == 0) {
+			userSearchInfo.errorMessage = 'error in searching with loginUsername: ' + userSearchInfo.loginUsername + ' with empty result';
+		} else if (userSearchInfo.loginUserDnCount > 1) {
+			userSearchInfo.errorMessage = 'error in searching with loginUsername: ' + userSearchInfo.loginUsername + ' with ' + userSearchInfo.loginUserDnCount + ' result';
+		}
+	}
+	if (userSearchInfo.errorMessage != null) {
+		console.error(userSearchInfo.errorMessage);
+		return false;
+	}
+	return true;
+}
+
+function createOnLdapSearchErrorHandler(userSearchInfo) {
+	return function (error) {
+		userSearchInfo.errorMessage = 'error in searching with loginUsername: ' + userSearchInfo.loginUsername + ' with error ' + error.message;
+	}
 }
 
 function createOnBindLoginUserHandler(client, res, userSearchInfo) {
@@ -102,56 +211,27 @@ function createOnBindLoginUserHandler(client, res, userSearchInfo) {
 	}
 }
 
-function createOnLdapSearchEndHandler(client, res, userSearchInfo) {
-	return function (result) {
-		if (userSearchInfo.searchErrorMessage != null) {}
-		else if (result.status != 0) {
-			userSearchInfo.searchErrorMessage = 'error in searching with loginUsername: ' + userSearchInfo.loginUsername + ' with status' + result.status;
-		} else if (userSearchInfo.loginUserDnCount == 0) {
-			userSearchInfo.searchErrorMessage = 'error in searching with loginUsername: ' + userSearchInfo.loginUsername + ' with empty result';
-		} else if (userSearchInfo.loginUserDnCount > 1) {
-			userSearchInfo.searchErrorMessage = 'error in searching with loginUsername: ' + userSearchInfo.loginUsername + ' with ' + userSearchInfo.loginUserDnCount + ' result';
-		}
-		if (userSearchInfo.searchErrorMessage != null) {
-			unbindLdap(client);
-			console.error(userSearchInfo.searchErrorMessage);
-			res.send(userSearchInfo.searchErrorMessage);
-		} else {
-			console.log('Binding using DN: ' + userSearchInfo.loginUserDn);
-			//                  var clientForLogin = ldap.createClient({
-			//                    url: 'ldap://'+ldapHost+':'+ldapPort,
-			//                    timeout: 5000,
-			//                    connectTimeout: 10000
-			//                  });
-			client.bind(userSearchInfo.loginUserDn, userSearchInfo.loginPassword, createOnBindLoginUserHandler(client, res, userSearchInfo));
-		}
-	}
-}
-function createOnLdapSearchErrorHandler(userSearchInfo) {
-	return function (error) {
-		userSearchInfo.searchErrorMessage = 'error in searching with loginUsername: ' + userSearchInfo.loginUsername + ' with error ' + error.message;
-	}
-}
-
 app.get('/test', function (req, res) {
 	var systemUsername = 'OAuthTestUser1';
 	var systemPassword = 'OAuthTestUser1';
 	var systemDnSuffix = 'OU=IMT,OU=CLK,OU=HQ,OU=Users,OU=CPA,DC=nwow001,DC=corp,DC=ete,DC=cathaypacific,DC=com';
 	var ldapHost = 'ADDS.ETE.CATHAYPACIFIC.COM';
 	var ldapPort = '389';
-	var loginUsername = 'OAuthTestUser1';
-	var loginPassword = 'OAuthTestUser1';
 	var loginBaseDn = 'OU=IMT,OU=CLK,OU=HQ,OU=Users,OU=CPA,DC=nwow001,DC=corp,DC=ete,DC=cathaypacific,DC=com';
 
-	if (req.body.username != null) {
-		loginUsername = req.body.username;
-	} else if (req.headers.username != null) {
-		loginUsername = req.headers.username;
+	var userSearchInfo = {
+		loginUsername: extractUsernameFromRequest(req),
+		loginPassword: extractPasswordFromRequest(req),
+		loginBaseDn: null;
+		loginUserDnCount: 0,
+		loginUserDn: null,
+		errorMessage: null
 	}
-	if (req.body.password != null) {
-		loginPassword = req.body.password;
-	} else if (req.headers.password != null) {
-		loginPassword = req.headers.password;
+	userSearchInfo.loginBaseDn=loginBaseDn;
+	
+	if (!assertUsernamePassword(userSearchInfo)) {
+		res.send(userSearchInfo.errorMessage);
+		return;
 	}
 
 	var client = ldap.createClient({
@@ -161,59 +241,11 @@ app.get('/test', function (req, res) {
 		});
 
 	try {
-		client.bind('cn=' + systemUsername + ',' + systemDnSuffix, systemPassword, function (error) {
-			if (error) {
-				console.log('Unable to bind with systemUsername: ' + systemUserName + ' with error ' + error.message);
-				unbindLdap(clietn);
-				res.send('Unable to bind with systemUsername: ' + systemUserName + ' with error ' + error.message);
-			} else {
-				console.log('connected');
-				var opts = {
-					filter: '(cn=' + loginUsername + ')',
-					scope: 'sub',
-					attributes: ['dn']
-				};
-				client.search(loginBaseDn, opts, function (error, search) {
-					console.log('Searching for ' + loginUsername);
-					if (error) {
-						console.log('Unable to search with loginUsername: ' + loginUsername + ' with error ' + error.message);
-						unbindLdap(clietn);
-						res.send('Unable to search with loginUsername: ' + loginUsername + ' with error ' + error.message);
-					} else {
-						var loginUserDn = null;
-						var loginUserDnCount = 0;
-						var searchErrorMessage = null;
-						
-						var userSearchInfo = {
-							loginUsername: null,
-							loginPassword: null,
-							loginUserDnCount: 0,
-							loginUserDn: null,
-							searchErrorMessage: null
-						}
-						userSearchInfo.loginUsername=loginUsername;
-						userSearchInfo.loginPassword=loginPassword;
-						search.on('searchEntry', function (entry) {
-							if (entry.object) {
-								loginUserDnCount++;
-								if (loginUserDnCount == 1) {
-									loginUserDn = entry.object.dn;
-								}
-								console.log('Found DN: ' + JSON.stringify(entry.object));
-
-							}
-						});
-
-						search.on('error', createOnLdapSearchErrorHandler(userSearchInfo));
-
-						search.on('end', createOnLdapSearchEndHandler(client, res, userSearchInfo));
-					}
-				});
-			}
-		});
+		client.bind('cn=' + systemUsername + ',' + systemDnSuffix, systemPassword,
+			createOnLdapBindHandler(client, res, systemUserName, userSearchInfo));
 	} catch (error) {
 		console.log(error);
-		unbindLdap(clietn);
+		unbindLdap(client);
 		res.send('error in binding: ' + error.message);
 	}
 
